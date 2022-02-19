8a2f716713952fc8f9cfca62ae38c48d67f3c02e
Cleiton Marques <cleiton.p.marques@gmail.com>
2022-02-19 17:44:00 +0000

    attachments database fix

--- src/models/audit_attachments.js
+++ src/models/audit_attachments.js
@@ -4,7 +4,9 @@ module.exports = function (sequelize, DataTypes) {
         audit_attachment_item_id: { type: DataTypes.INTEGER, allowNull: false, Comment: 'Auditoria que o anexo está associado.'},
         audit_attachment_description: {type: DataTypes.STRING(200),allowNull: true },
         audit_attachment_src: { type: DataTypes.STRING(268), allowNull: false },
-        audit_id: { type: DataTypes.INTEGER, allowNull: false , Comment: 'ID da auditoria que este item pertence'}
+        audit_id: { type: DataTypes.INTEGER, allowNull: false, Comment: 'ID da auditoria que este item pertence' },
+        createdAt: { type: DataTypes.DATE, allowNull: false },
+        updatedAt: { type: DataTypes.DATE, allowNull: true }
     }, {
         tableName: 'audit_attachments'
     });
