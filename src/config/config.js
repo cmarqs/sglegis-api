6669ab055c362bad841e56c21e5da531b37d9f6d
Cleiton Marques <cleiton.p.marques@gmail.com>
2022-04-03 16:40:03 +0100

    Atualizado padrão de conexao no config

--- src/config/config.js
+++ src/config/config.js
@@ -10,8 +10,12 @@ module.exports = {
         dialect: 'mysql',
     },
     homolog: {
-        database_url: process.env.DATABASE_URL,
-        dialect: 'mysql'
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: 'mysql',
     },
     production: {
         username: process.env.DATABASE_USER,
